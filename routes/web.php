edfb71e7447ee8451adca95457bd606748e09633
Nguyễn Văn Hiếu <hieuhanu208@gmail.com>
2021-04-08 10:14:21 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'RESUMO - FROM VIETNAM';
 });
+
+Route::get('/hello', function () {
+    return 'RESUMO - FROM VIETNAM';
+});
+
